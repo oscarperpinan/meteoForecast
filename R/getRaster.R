6dc634c075399495ecde4df0b6eb4c7c053eeef7
Oscar Perpiñán <oscar.perpinan@gmail.com>
2014-02-02 09:13:57 +0100

    Define PROJ.4 and extent of Raster for OpenMeteo

--- R/getRaster.R
+++ R/getRaster.R
@@ -33,14 +33,13 @@ getRaster <-
              ...){
 
         service <- match.arg(service, c('meteogalicia', 'openmeteo'))
-
+        ## Model initialization time
         run <- switch(service,
                       meteogalicia = match.arg(run, c('00', '12')),
                       openmeteo = match.arg(run, c('00', '06', '12', '18'))
                       )
-
         dd <- format(as.Date(day), format='%Y%m%d')
-    
+        ## Bounding box: it only works for meteogalicia
         if (!missing(box)) {
             ext <- extent(box)
             box <- paste0('&north=', ymax(ext),
@@ -49,7 +48,7 @@ getRaster <-
                           '&south=', ymin(ext))
         } else box <- ''
         
-
+        ## Name of files to be read/stored
         ncFile <- switch(service,
                          meteogalicia = paste0(paste(var, dd, run, 
                              sep='_'), '.nc'), 
@@ -62,6 +61,8 @@ getRaster <-
             on.exit(setwd(old))
             
             success <- switch(service,
+                              ## Meteogalicia provides a multilayer
+                              ## file with all the time frames
                               meteogalicia = {
                                   completeURL <- composeURL(var, dd, run,
                                                             box, '', 
@@ -69,6 +70,8 @@ getRaster <-
                                   try(download.file(completeURL, ncFile),
                                       silent=TRUE)
                               },
+                              ## OpenMeteo provides a different file
+                              ## for each time frame
                               openmeteo = 
                                   lapply(seq_along(frames), function(i) {
                                       completeURL <- composeURL(var, dd, run,
@@ -86,21 +89,37 @@ getRaster <-
             }
         } ## End of Remote
         
+        ## Read files
         suppressWarnings(bNC <- stack(ncFile))
-        b <- brick(bNC)
+        ## Convert into a RasterBrick
+        b <- switch(service,
+                    meteogalicia = brick(bNC),
+                    ## https://forum.openmeteodata.org/index.php?topic=33.msg96#msg96
+                    openmeteo = brick(nrow=309, ncol=495,
+                        nl=length(frames),
+                        xmn = -2963997.87057, ymn = -1848004.2008,
+                        xmx = 2964000.82884, ymx = 1848004.09676)
+                    )
+        ## Get values in memory to avoid problems with time index and
+        ## projection
         b[] <- getValues(bNC)
-        
+        ## Projection parameters are either not well defined in the
+        ## NetCDF files or incorrectly read by raster
         projection(b) <- switch(service,
-                                meteogalicia = "+proj=lcc +lon_0=-14.1 +lat_0=34.823 +lat_1=43 +lat_2=43 +x_0=536402.3 +y_0=-18558.61 +units=km +ellps=WGS84",
-                                openmeteo = "+proj=lcc +lon_0=4 +lat_1=47.5 +lat_2=47.5"
+                                ## Provided by gdalsrsinfo
+                                meteogalicia = "+proj=lcc +lat_1=43 +lat_2=43 +lat_0=34.82300186157227 +lon_0=-14.10000038146973 +x_0=536402.34 +y_0=-18558.61 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=km +no_defs",
+                                ## https://forum.openmeteodata.org/index.php?topic=33.msg96#msg96
+                                openmeteo = "+proj=lcc +lon_0=4 +lat_0=47.5 +lat_1=47.5 +lat_2=47.5 +a=6370000. +b=6370000. +no_defs"
                                 )
-        
+        ## Time index
         hours <- switch(service,
-                        meteogalicia = (1:96) * 3600, 
+                        meteogalicia = (seq_length(nlayers(b))) * 3600, 
                         openmeteo = frames * 3600 
                         )
         tt <- hours + as.numeric(run)*36 + as.POSIXct(day)
         b <- setZ(b, tt)
+        ## Names
         if (missing(names)) names(b) <- format(tt, 'D%Y-%m-%d_H%H')
+        ## Here it goes!
         b
     }
