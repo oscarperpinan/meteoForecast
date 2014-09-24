8f4becfbba4af42ac847c18a9e91d558946e5837
Oscar Perpiñán Lamigueiro <oscar@IES-INVERSOR.upm.es>
2014-09-24 14:00:46 +0200

    getRasterDay works!, and getRasterDays relies on it.

--- R/getRasterDay.R
+++ R/getRasterDay.R
@@ -3,31 +3,24 @@ getRasterDay <- function(var = 'swflx', day = Sys.Date(),
                          remote = TRUE, service = 'meteogalicia',
                          dataDir = '.', ...){
   
-  ## The possibilities for src can be expanded in the future
-  runs <- switch(service, meteogalicia = '00')
-  
-  ## Time difference between runs
-  delta <- 24
-  ## Specific for runs '00' of meteogalicia
-  
+  day <- as.Date(day)
+  today <- Sys.Date()
+
   if(!remote) {
       old <- setwd(dataDir)
       on.exit(setwd(old))
   }
-  
-  ### Read the first "delta" frames from each run to compose a list (zl)
-  zl <- lapply(runs, FUN=function(run){
-               r <- try(getRaster(var, day, run=run, frames=delta,
-                              service=service, remote=remote, ...))
-               })
-  isOK <- sapply(zl, FUN = function(x) class(x)!='try-error')
-  
-  ### RasterBrick joining the elements of the list (zl)
-  z <- brick(stack(zl[isOK]))
-  
-  ### Adding time index
-  tt <- do.call(c, lapply(zl[isOK], getZ))
-  attr(tt, 'tzone') <- 'UTC'
-  z <- setZ(z, tt)
-  
+
+  if (day > today) {
+      r <- getRaster(var = var, day = today,
+                     frames = 'complete', service = service,
+                     remote = remote, ...)
+      idx <- which(day == as.Date(getZ(r)))
+      r[[idx]]
+  } else {
+      r <- getRaster(var = var, day = day,
+                     frames = 24, service = service,
+                     remote = remote, ...)
+      r
+  }
 }
--- R/getRasterDays.R
+++ R/getRasterDays.R
@@ -11,24 +11,9 @@ getRasterDays <- function(var = 'swflx',
     start <- as.Date(start)
     if (missing(end)) {
         getRaster(var, day = start, run='00', ...)
-    } else if(start == Sys.Date()+1) {
-        lr <- try(suppressMessages(getRaster(var, day = start-1,
-                                             run = '00', frames = 48,
-                                             remote = remote,
-                                             dataDir = '.',
-                                             ...)
-                  ))
-        if(class(lr)!='try-error'){
-            lr <- subset(lr, 25:48)
-            tt <- getZ(lr)
-            attr(tt, 'tzone') <- 'UTC'
-            s <- setZ(lr, tt)
-            s
-        }
     } else {
         end <- as.Date(end)
         stopifnot(end >= start)
-        stopifnot(end <= Sys.Date())
         days <- seq(start, end, by='day')
         ## Progress bar is only enabled if end > start
         havePB <- (end > start)
@@ -38,11 +23,10 @@ getRasterDays <- function(var = 'swflx',
         lr <- lapply(days, FUN = function(d) {
             if (havePB) setTxtProgressBar(pb, as.numeric(d))
             try(suppressMessages(
-                getRaster(var, day = d,
-                          run = '00', frames = 24,
-                          remote = remote,
-                          dataDir = '.',
-                          ...)
+                getRasterDay(var, day = d,
+                             remote = remote,
+                             dataDir = '.',
+                             ...)
                 ))
         })
         if (havePB) close(pb)
