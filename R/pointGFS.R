384158ff3928806b06bfc9ed1c9b6fb565f77526
Oscar Perpiñán Lamigueiro <oscar@IES-INVERSOR.upm.es>
2014-09-24 12:11:39 +0200

    Corrects GFS projection in getPoint, related to #4

--- R/pointGFS.R
+++ R/pointGFS.R
@@ -1,13 +1,14 @@
 pointInGFS <-
     function(lon, lat){
-        box <- extent(0, 360, -90, 90)
+        box <- extent(-180, 180, -90, 90)
         isInside(lon, lat, box)
     }
 
 pointGFS <- function(lon, lat, vars,
                      day=Sys.Date(), run='00', ...){
     if (!pointInGFS(lon, lat)) stop('Point outside GFS region.')
-    
+    ## We use longitudes in the range -180..180, but GFS needs 0..360
+    lon <- ifelse(lon > 0, lon, 360 + lon)
     frames <- seq(0, 192, by = 3)
     gfsFiles <- lapply(frames, FUN = function(tt){
         completeURL <- composeURL(vars, day, run,
@@ -22,15 +23,22 @@ pointGFS <- function(lon, lat, vars,
     })
     ## remove NULL elements
     gfsFiles <- do.call(c, gfsFiles)
-    z <- do.call("rbind", lapply(gfsFiles, read.csv, header = TRUE))
-    idx <- as.POSIXct(z[,1], format='%Y-%m-%dT%H:%M:%SZ', tz = 'UTC')
-    lat <- as.numeric(as.character(z[1, 2]))
-    lon <- as.numeric(as.character(z[1, 3]))
-    z <- zoo(z[, -c(1, 2, 3)], idx)
-    
-    names(z) <- vars
-    attr(z, 'lat') <- lat
-    attr(z, 'lon') <- lon
-    message('Files available at ', tempdir())
-    z
+
+    if (is.null(gfsFiles)) {
+        stop(' no data could be retrieved. Check date, coordinates and variable(s).')
+    } else {
+        z <- do.call("rbind", lapply(gfsFiles, read.csv, header = TRUE))
+        idx <- as.POSIXct(z[,1], format='%Y-%m-%dT%H:%M:%SZ', tz = 'UTC')
+        lat <- as.numeric(as.character(z[1, 2]))
+        ## GFS returns longitude in the -180..180 range, no additional
+        ## conversion is needed.
+        lon <- as.numeric(as.character(z[1, 3]))
+        z <- zoo(z[, -c(1, 2, 3)], idx)
+        
+        names(z) <- vars
+        attr(z, 'lat') <- lat
+        attr(z, 'lon') <- lon
+        message('Files available at ', tempdir())
+        z
+    }
 }
