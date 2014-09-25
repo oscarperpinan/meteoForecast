4227e1e4173eb54860b55ddf9d17dcf61421fe3d
Oscar Perpiñán Lamigueiro <oscar@OPL-EUITI.upm.es>
2014-09-25 10:42:23 +0200

    Some comments in getRasterDay

--- R/getRasterDay.R
+++ R/getRasterDay.R
@@ -10,7 +10,9 @@ getRasterDay <- function(var = 'swflx', day = Sys.Date(),
       old <- setwd(dataDir)
       on.exit(setwd(old))
   }
-
+  ## If I need a day in the future, I have to download the most recent
+  ## file (today) and extract the frames that correspond to the day we
+  ## need.
   if (day > today) {
       r <- getRaster(var = var, day = today,
                      frames = 'complete', service = service,
@@ -18,6 +20,8 @@ getRasterDay <- function(var = 'swflx', day = Sys.Date(),
       idx <- which(day == as.Date(getZ(r)))
       r[[idx]]
   } else {
+      ## If the day is in the past, use `getRaster` to obtain only the
+      ## 24 frames that correspond to that day.
       r <- getRaster(var = var, day = day,
                      frames = 24, service = service,
                      remote = remote, ...)
