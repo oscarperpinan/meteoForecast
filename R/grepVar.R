2d15cba8f00e1ad379456b45abaaa9d4ddbcada4
Oscar Perpiñán Lamigueiro <oscar@OPL-EUITI.upm.es>
2015-05-21 12:32:25 +0200

    LazyData in DESCRIPTION

--- R/grepVar.R
+++ R/grepVar.R
@@ -1,13 +1,11 @@
 grepVar <- function(x, service, complete = FALSE){
     varsFile <- switch(service,
-                   meteogalicia = 'varsMG',
-                   gfs = 'varsGFS',
-                   nam = 'varsNAM',
-                   rap = 'varsRAP',
+                       meteogalicia = 'varsMG',
+                       openmeteo = 'varsOM',
+                       gfs = 'varsGFS',
+                       nam = 'varsNAM',
+                       rap = 'varsRAP',
                        stop('Unknown service.'))
-    ## Load the corresponding file
-    data(list = varsFile)
-    ## Read it
     vars <- eval(parse(text = varsFile))
     idx <- grep(x, vars$label, ignore.case=TRUE)
     if (isTRUE(complete)) vars[idx,]
